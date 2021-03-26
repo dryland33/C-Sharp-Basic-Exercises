8a98b09d7c94f4eee360304045da52955953960e
Rylan Wassem <dryland28@gmail.com>
2021-03-26 13:52:15 -0400

    solved problem 18

--- Program.cs
+++ Program.cs
@@ -21,7 +21,9 @@ namespace Basic_Exercises
             //RectNum();
             //TempConv();
             //DelChar();
-            LetSwap();
+            //LetSwap();
+            //TackOn();
+            OneOfEach();
         }
 
 
@@ -338,12 +340,21 @@ namespace Basic_Exercises
         }
 
         /*
-        17. Write a C# program to create a new string from a given string (length 1 or more ) with the first character added at the front and back. Go to the editor
+        17. Write a C# program to create a new string from a given string (length 1 or more ) with 
+        the first character added at the front and back. Go to the editor
         Sample Output:
         Input a string : The quick brown fox jumps over the lazy dog.
         TThe quick brown fox jumps over the lazy dog.T
         */
-        
+        static void TackOn() {
+            Console.Write("Enter some text: ");
+            string text = Console.ReadLine();
+            char first = text.First();
+            text += first;
+            text = first + text;
+            Console.Write($"With the first letter added front and back: {text}");
+        }
+
         /*
         18. Write a C# program to check two given integers and return true if one is negative and one is positive. Go to the editor
         Sample Output:
@@ -353,35 +364,58 @@ namespace Basic_Exercises
         25
         Check if one is negative and one is positive:
         True
-        *//*
+        */
+        static void Swap(ref int int1, ref int int2) {
+            int temp = int1;
+            int1 = int2;
+            int2 = temp;
+        }
+        static void OneOfEach() {
+            Console.Write("Enter an interger: ");
+            int int1 = int.Parse(Console.ReadLine());
+            Console.Write("Enter another integer: ");
+            int int2 = int.Parse(Console.ReadLine());
+            //Put the larger one first
+            if (int1 < int2) Swap(ref int1, ref int2);
+            //if absolute value of addition is greater than larger of two then signs are the same
+            if (Math.Abs(int1 + int2) >= int1) Console.WriteLine("False");
+            else Console.WriteLine("True");
+        }
 
+        /*
         19. Write a C# program to compute the sum of two given integers, if two values are equal then return the triple of their sum. Go to the editor
-        *//*
+        */
 
+        /*
         20. Write a C# program to get the absolute value of the difference between two given numbers. Return double the absolute value of the difference if the first number is greater than second number. Go to the editor
-        *//*
+        */
 
+        /*
         21. Write a C# program to check the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20. Go to the editor
-        *//*
+        */
 
+        /*
         22. Write a C# program to check if an given integer is within 20 of 100 or 200. Go to the editor
         Sample Output:
         Input an integer:
         25
         False
-        *//*
+        */
 
+        /*
         23. Write a C# program to convert a given string into lowercase. Go to the editor
         Sample Output:
         write a c# sharp program to display the following pattern using the alphabet.
-       *//*
+       */
 
+        /*
         24. Write a C# program to find the longest word in a string. Go to the editor
         Test Data: Write a C# Sharp Program to display the following pattern using the alphabet.
         Sample Output:
         following
-        *//*
+        */
 
+        /*
         25. Write a C# program to print the odd numbers from 1 to 99. Prints one number per line. Go to the editor
         Sample Output:
         Odd numbers from 1 to 99. Prints one number per line.
@@ -394,59 +428,68 @@ namespace Basic_Exercises
         95
         97
         99
-        *//*
+        */
 
+        /*
         26. Write a C# program to compute the sum of the first 500 prime numbers. Go to the editor
         Sample Output:
         Sum of the first 500 prime numbers:
         824693
-        *//*
+        */
 
+        /*
         27. Write a C# program and compute the sum of the digits of an integer. Go to the editor
         Sample Output:
         Input a number(integer): 12
         Sum of the digits of the said integer: 3
-        *//*
+        */
 
+        /*
         28. Write a C# program to reverse the words of a sentence. Go to the editor
         Sample Output:
         Original String: Display the pattern like pyramid using the alphabet.
         Reverse String: alphabet. the using pyramid like pattern the Display
-        *//*
+        */
 
+        /*
         29. Write a C# program to find the size of a specified file in bytes. Go to the editor
         Sample Output:
         Size of a file: 31
-        *//*
+        */
 
+        /*
         30. Write a C# program to convert a hexadecimal number to decimal number. Go to the editor
         Sample Output:
         Hexadecimal number: 4B0
         Convert to-
         Decimal number: 1200
-        *//*
+        */
 
+        /*
         31. Write a C# program to multiply corresponding elements of two arrays of integers. Go to the editor
         Sample Output:
         Array1: [1, 3, -5, 4]
         Array2: [1, 4, -5, -2]
         Multiply corresponding elements of two arrays:
         1 12 25 -8
-        *//*
+        */
 
+        /*
         32. Write a C# program to create a new string of four copies, taking last four characters from a given string. If the length of the given string is less than 4 return the original one. Go to the editor
         Sample Output:
         Input a string : The quick brown fox jumps over the lazy dog.
         dog.dog.dog.dog.
-        *//*
+        */
 
+        /*
         33. Write a C# program to check if a given positive number is a multiple of 3 or a multiple of 7. Go to the editor
         Sample Output:
         Input first integer:
         15
         True
-        *//*
+        */
 
+        /*
         34. Write a C# program to check if a string starts with a specified word. Go to the editor
         Note: Suppose the sentence starts with "Hello"
         Sample Data: string1 = "Hello how are you?"
@@ -454,34 +497,39 @@ namespace Basic_Exercises
         Sample Output:
         Input a string : Hello how are you?
         True
-        *//*
+        */
 
+        /*
         35. Write a C# program to check two given numbers where one is less than 100 and other is greater than 200. Go to the editor
         Sample Output:
         Input a first number(<100): 75
         Input a second number(>100): 250
         True
-        *//*
+        */
 
+        /*
         36. Write a C# program to check if an integer (from the two given integers) is in the range -10 to 10. Go to the editor
         Sample Output:
         Input a first number: -5
         Input a second number: 8
         True
-        *//*
+        */
 
+        /*
         37. Write a C# program to check if "HP" appears at second position in a string and returns the string without "HP". Go to the editor
         Test Data: PHP Tutorial
         Sample Output:
         P Tutorial
-        *//*
+        */
 
+        /*
         38. Write a C# program to get a new string of two characters from a given string. The first and second character of the given string must be "P" and "H", so PHP will be "PH". Go to the editor
         Test Data: PHP
         Sample Output:
         PH
-        *//*
+        */
 
+        /*
         39. Write a C# program to find the largest and lowest values from three integer values. Go to the editor
         Test Data:
         Input first integer:
@@ -493,8 +541,9 @@ namespace Basic_Exercises
         Sample Output
         Largest of three: 30
         Lowest of three: 15
-        *//*
+        */
 
+        /*
         40. Write a C# program to check the nearest value of 20 of two given integers and return 0 if two numbers are same. Go to the editor
         Test Data:
         Input first integer:
@@ -503,65 +552,74 @@ namespace Basic_Exercises
         12
         Sample Output
         15
-        *//*
+        */
 
+        /*
         41. Write a C# program to check if a given string contains ‘w’ character between 1 and 3 times. Go to the editor
         Test Data:
         Input a string (conatins at least one 'w' char) : w3resource
         Test the string contains 'w' character between 1 and 3 times:
         Sample Output
         True
-        *//*
+        */
 
+        /*
         42. Write a C# program to create a new string where the first 4 characters will be in lower case. If the string is less than 4 characters then make the whole string in upper case. Go to the editor
         Test Data:
         Input a string: w3r
         Sample Output
         W3R
-        *//*
+        */
 
+        /*
         43. Write a C# program to check if a given string starts with "w" and immediately followed by two "ww". Go to the editor
         Test Data:
         Input a string : www
         Sample Output
         False
-        *//*
+        */
 
+        /*
         44. Write a C# program to create a new string of every other character (odd position) from the first position of a given string. Go to the editor
         Test Data:
         Input a string : w3resource
         Sample Output
         wrsuc
-        *//*
+        */
 
+        /*
         45. Write a C# program to count a specified number in a given array of integers. Go to the editor
         Test Data:
         Input an integer: 5
         Sample Output
         Number of 5 present in the said array: 2
-        *//*
+        */
 
+        /*
         46. Write a C# program to check if a number appears as either the first or last element of an array of integers and the length is 1 or more. Go to the editor
         Test Data:
         Input an integer: 25
         Sample Output
         False
-        *//*
+        */
 
+        /*
         47. Write a C# program to compute the sum of all the elements of an array of integers. Go to the editor
         Test Data:
         Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
         Sample Output
         Sum: 69
-        *//*
+        */
 
+        /*
         48. Write a C# program to check if the first element and the last element are equal of an array of integers and the length is 1 or more. Go to the editor
         Test Data:
         Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
         Sample Output
         True
-        *//*
+        */
 
+        /*
         49. Write a C# program to check if the first element or the last element of the two arrays ( length 1 or more) are equal. Go to the editor
         Test Data:
         Array1: [1, 2, 2, 3, 3, 4, 5, 6, 5, 7, 7, 7, 8, 8, 1]
@@ -569,56 +627,67 @@ namespace Basic_Exercises
         Check if the first element or the last element of the two arrays ( leng th 1 or more) are equal.
         Sample Output
         True
-        *//*
+        */
 
+        /*
         50. Write a C# program to rotate an array (length 3) of integers in left direction. Go to the editor
         Test Data:
         Array1: [1, 2, 8]
         After rotating array becomes: [2, 8, 1]
-        *//*
+        */
 
+        /*
         51. Write a C# program to get the larger value between first and last element of an array (length 3) of integers. Go to the editor
         Test Data:
         Array1: [1, 2, 5, 7, 8]
         Highest value between first and last values of the said array: 8
-        *//*
+        */
 
+        /*
         52. Write a C# program to create a new array of length containing the middle elements of three arrays (each length 3) of integers. Go to the editor
         Test Data:
         Array1: [1, 2, 5]
         Array2: [0, 3, 8]
         Array3: [-1, 0, 2]
         New array: [2, 3, 0]
-        *//*
+        */
 
+        /*
         53. Write a C# program to check if an array contains an odd number. Go to the editor
         Test Data:
         Original array: [2, 4, 7, 8, 6]
         Check if an array contains an odd number? True
-        *//*
+        */
 
+        /*
         54. Write a C# program to get the century from a year. Go to the editor
-        *//*
+        */
 
+        /*
         55. Write a C# program to find the pair of adjacent elements that has the largest product of an given array which is equal to a given value. Go to the editor
-        *//*
+        */
 
+        /*
         56. Write a C# program to check if a given string is a palindrome or not. Go to the editor
         Sample Example:
         For 'aaa' the output should be true
         For 'abcd' the output should be false
-        *//*
+        */
 
+        /*
         57. Write a C# program to find the pair of adjacent elements that has the highest product of an given array of integers. Go to the editor
-        *//*
+        */
 
+        /*
         58. Write a C# program which will accept a list of integers and checks how many integers are needed to complete the range. Go to the editor
         Sample Example [1, 3, 4, 7, 9], between 1-9 -> 2, 5, 6, 8 are not present in the list. So output will be 4.
-        *//*
+        */
 
+        /*
         59. Write a C# program to check whether it is possible to create a strictly increasing sequence from a given sequence of integers as an array. Go to the editor
-        *//*
+        */
 
+        /*
         60. Write a C# program to calculate the sum of all the intgers of a rectangular matrix except those integers which are located below an intger of value 0. Go to the editor
         Sample Example:
         matrix = [[0, 2, 3, 2],
@@ -630,7 +699,7 @@ namespace Basic_Exercises
         [X, X, X, X]]
         Therefore sum will be: 2 + 3 + 2 + 6 + 1 = 14
         */
-        
+
         /*
         61. Write a C# program to sort the integers in ascending order without moving the number -5. Go to the editor
         */
